b841d8acf9a5bef90d56dd5a009d01198e5e97a0
nkjammula <nkjammula@users.noreply.github.com>
2020-04-13 15:04:52 +0000

    deploy: f23f67f06606ed87ecf0d179f9e022ba5cf9893b

---
+++ precache-manifest.cf6a5b1cdfa1cfea99b6917f7e164a19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "932f94b6272d5b2e427221d8220cc6db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ab96d673c12387ab4cf",
+    "url": "/static/css/2.2fbe55bd.chunk.css"
+  },
+  {
+    "revision": "e3261c6b0d444027d2af",
+    "url": "/static/css/main.63ab0928.chunk.css"
+  },
+  {
+    "revision": "9ab96d673c12387ab4cf",
+    "url": "/static/js/2.ebb4c015.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ebb4c015.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3261c6b0d444027d2af",
+    "url": "/static/js/main.9e79f910.chunk.js"
+  },
+  {
+    "revision": "8d8d139b190630b6bc42",
+    "url": "/static/js/runtime-main.7ba82ef7.js"
+  }
+]);
