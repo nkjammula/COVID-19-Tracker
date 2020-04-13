b29c60bf4bc3e192728f67e8e541c099c6f366cc
naveen jammula <noblenaveen@gmail.com>
2020-04-13 10:27:32 -0500

    Updates

---
+++ precache-manifest.2e8852a2661ea4a46a2c002cad6359bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9af1657213d7c58fa9d5ffe8f8b8a5b5",
+    "url": "/Covid19Tracker/index.html"
+  },
+  {
+    "revision": "9ab96d673c12387ab4cf",
+    "url": "/Covid19Tracker/static/css/2.2fbe55bd.chunk.css"
+  },
+  {
+    "revision": "b8dbeeb5ab3fa52e4a83",
+    "url": "/Covid19Tracker/static/css/main.63ab0928.chunk.css"
+  },
+  {
+    "revision": "9ab96d673c12387ab4cf",
+    "url": "/Covid19Tracker/static/js/2.ebb4c015.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Covid19Tracker/static/js/2.ebb4c015.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8dbeeb5ab3fa52e4a83",
+    "url": "/Covid19Tracker/static/js/main.4ab12494.chunk.js"
+  },
+  {
+    "revision": "27b5e811f53f62a51943",
+    "url": "/Covid19Tracker/static/js/runtime-main.8d6ca865.js"
+  }
+]);
